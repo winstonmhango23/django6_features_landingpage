615b8aecf5418c5ae0b7e265a719fc36999de4a2
winstonmhango23 <winstonmhango23@gmail.com>
2025-11-04 14:03:02 +0200

    Improve root manage.py to properly delegate to landingpage directory

--- manage.py
+++ manage.py
@@ -1,7 +1,25 @@
 #!/usr/bin/env python
+"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
+
+def main():
+    """Run administrative tasks."""
+    # Change to the landingpage directory where the actual manage.py is located
+    os.chdir(os.path.join(os.path.dirname(__file__), "landingpage"))
+    
+    # Import and run the actual Django manage.py
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(sys.argv)
+
+
 if __name__ == "__main__":
-    os.chdir("landingpage")
-    os.execv(sys.executable, [sys.executable, "manage.py"] + sys.argv[1:])
+    main()
